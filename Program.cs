4faf08d097ccb391d637262bfc935f9da0bc6f9b
Hernan Quiroga <quiroga.hernan@gmail.com>
2022-06-05 19:41:22 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Ingrese el tamaño del vector");
+var largoArray = Console.ReadLine();  // Ingreso el tamaño del vector
+int n = Convert.ToInt32(largoArray); // Convierto a entero   
+
+int[] miVector = new int[n];    // Declaro el vector
+
+Random r = new Random();    // Genero un numero al azar
+
+for (int i = 0; i < miVector.Length; i++) // Guardo los valores generados por Random(menores a 100) en el array.
+{
+    miVector[i] = r.Next(100);
+}
+
+
+foreach (int item in miVector) { // Muestro los valores del vector
+    Console.WriteLine(item); 
+}
+
+//Con los conocimientos vistos hasta ahora en clase realizar un programa que haga lo siguiente:
+
+//1) Pedir al usuario la longitud de un vector
+//2) Crear el vector del tamaño ingresado.
+//3) Llenar el mismo con datos aleatorios
+//4) Mostrar el vector por pantalla
+//5) Invertir el vector, de manera que el primer elemento quede al último y el útimo en el primero; el segundo en anteúltimo,
+//el anteúltimo en el segundo y así sucesivamente.
+//  En otra palabras si el vector es de 5 posiciones y el usuario ingresó: 10, 20, 30, 40, 50, una vez invertido,
+//  el vector debe quedar así: 50, 40, 30, 20, 10.
+//  Se debe usar solo lo visto en clase hasta ahora y no los métodos que trae C# para estas cuestiones.
+//  Tampoco se debe crear un vector nuevo o auxiliar para realizar el ejercicio. 
+//6) Mostrar el vector nuevamente.
